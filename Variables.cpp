09d4e87137ab446bb30632d96091f4a4e23d458b
langatfestus <langatfestus94@gmail.com>
2023-05-27 12:39:10 +0300

    done

---
+++ Variables.cpp
@@ -0,0 +1,15 @@
+// Defining and initializing variables
+#include <iostream>
+using namespace std;
+int main()
+{
+float x = 123.456F, // or double
+y = 76.543F,
+sum;
+sum = x + y;
+cout << "Total: "
+<< x << " + " << y << " = " << sum << endl;
+cout << "Difference: "
+<< x << " — " << y << " = " << (x — y) << endl;
+return 0;
+}
